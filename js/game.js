c8291f4270fd0f8adf9198bda5913c3ab8c5c925
Jose Copeti <jrcopeti@gmail.com>
2024-06-28 10:36:51 +0200

    iteration 1 and 2 done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,36 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.player = null;
+    this.height = 600;
+    this.width = 500;
+    this.obstacle = [];
+    this.score = 0;
+    this.lives = 3;
+    this.gameIsOver = false;
+    this.gameIntervalId = null;
+    this.gameLoopFrequency = Math.round(1000 / 60);
+  }
+
+  start() {
+    this.gameScreen.style.width = this.width + "px";
+    this.gameScreen.style.height = this.height + "px";
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    console.log("in the game loop")
+    this.update()
+    this.gameIsOver && clearInterval(this.gameIntervalId)
+  }
+
+  update() {
+    console.log("in the update")
+  }
 }
